13ce235fad983c402996fcefd8c79ab9f92e7a14
Sofiakeser <skeser@udesa.edu.ar>
2025-10-26 16:15:30 -0300

    practica intro a react native

---
+++ Clase08/practica1/src/screens/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import BotonSaludar from '../components/BotonSaludar';
+import Contador from '../components/Contador';
+
+function Home(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hola mundo!</Text>
+            <BotonSaludar />
+            <Contador />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10
+    },
+    text: {
+        textAlign: 'center',
+    }
+})
+
+export default Home;
